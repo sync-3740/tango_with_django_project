d84b6bc00392e7aa708e0aa03e1b9d2dbbe049bc
Jonathan Kent <2466629k@student.gla.ac.uk>
2021-01-28 17:08:47 +0000

    End of Chapter 4

---
+++ rango/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    
+    context_dict = {'boldmessage': 'This tutorial has been put together by Jonathan Kent'}
+    
+    return render(request, 'rango/about.html', context=context_dict)
