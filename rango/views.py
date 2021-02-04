28162106eb572ced158a05d294f2a6c9e018dd89
Jonathan Kent <2466629k@student.gla.ac.uk>
2021-02-04 16:56:39 +0000

    End of chapter 5

--- rango/views.py
+++ rango/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rango.models import Category
 
 def index(request):
+
+    category_list = Category.objects.order_by('-likes')[:5]
 
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
 
